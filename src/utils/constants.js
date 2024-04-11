ecd5021e3a416ccc662a990cdbfeb16167fc72a8
abenhamouda <abenhamouda@iobeya.com>
2024-04-11 22:56:58 +0100

    🦺 enhance labels of charts

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,10 +7,10 @@ export const WORD_HIGHLIGHTER_ACTION = 'word-highlighter-action';
 
 export const confidanceToColorMap = [
   { check: (confidance) => confidance < 60, color: error.darker, label: '< 60%' },
-  { check: (confidance) => confidance < 70, color: error.main, label: '< 70%' },
-  { check: (confidance) => confidance < 80, color: error.light, label: '< 80%' },
-  { check: (confidance) => confidance < 90, color: warning.main, label: '< 90%' },
-  { check: (confidance) => confidance <= 100, color: success.main, label: '<= 100%'},
+  { check: (confidance) => confidance < 70, color: error.main, label: '61% - 70%' },
+  { check: (confidance) => confidance < 80, color: error.light, label: '71% - 80%' },
+  { check: (confidance) => confidance < 90, color: warning.main, label: '81% - 90%' },
+  { check: (confidance) => confidance <= 100, color: success.main, label: '91% - 100%'},
 ];
 
 export const confidanceColors = confidanceToColorMap.map(({ color }) => color);
