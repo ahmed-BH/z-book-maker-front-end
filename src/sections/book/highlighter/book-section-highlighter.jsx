d8ae92890f4798b153270e203ce464ad55b81090
abenhamouda <abenhamouda@iobeya.com>
2024-04-11 15:52:32 +0100

    💄 display tootip indicating the confidence of the highlighted section at mouse position

--- src/sections/book/highlighter/book-section-highlighter.jsx
+++ src/sections/book/highlighter/book-section-highlighter.jsx
@@ -1,16 +1,18 @@
 import { useMemo } from 'react';
 import PropTypes from 'prop-types';
 
+import Tooltip from '@mui/material/Tooltip';
+
 import { OCRUtils } from '../common/ocr-utils';
 import { error, success, warning } from '../../../theme/palette';
 import { LINE_HIGHLIGHTER_ACTION, WORD_HIGHLIGHTER_ACTION, PARAGRAPHIGH_HLIGHTER_ACTION } from '../../../utils/constants';
 
 const confidanceToColorMap = [
-  { check: (confidance) => confidance < 60  , color: error.darker },
-  { check: (confidance) => confidance < 70  , color: error.main },
-  { check: (confidance) => confidance < 80  , color: error.light },
-  { check: (confidance) => confidance < 90  , color: warning.main },
-  { check: (confidance) => confidance <= 100 , color: success.main},
+  { check: (confidance) => confidance < 60, color: error.darker },
+  { check: (confidance) => confidance < 70, color: error.main },
+  { check: (confidance) => confidance < 80, color: error.light },
+  { check: (confidance) => confidance < 90, color: warning.main },
+  { check: (confidance) => confidance <= 100, color: success.main },
 ]
 
 export function BookSectionHighlighter({ highlightAction, container, textBlocks }) {
@@ -38,13 +40,25 @@ export function BookSectionHighlighter({ highlightAction, container, textBlocks 
     let sections = []
     if (highlightAction === PARAGRAPHIGH_HLIGHTER_ACTION) {
       const paragraphs = textBlocks[0]?.paragraphs || [];
-      sections = paragraphs.map((paragraph) => (<div style={getRectangle(paragraph, container)} />));
+      sections = paragraphs.map((paragraph) => (
+        <Tooltip followCursor title={`${Math.ceil(paragraph.confidence)}%`}>
+          <div style={getRectangle(paragraph, container)} />
+        </Tooltip>
+      ));
     } else if (highlightAction === LINE_HIGHLIGHTER_ACTION) {
       const lines = OCRUtils.flattenParagraphLines(textBlocks)
-      sections = lines.map((line) => (<div style={getRectangle(line, container)} />));
+      sections = lines.map((line) => (
+        <Tooltip followCursor title={`${Math.ceil(line.confidence)}%`}>
+          <div style={getRectangle(line, container)} />
+        </Tooltip>
+      ));
     } else if (highlightAction === WORD_HIGHLIGHTER_ACTION) {
       const words = OCRUtils.flattenParagraphWords(textBlocks)
-      sections = words.map((word) => (<div style={getRectangle(word, container)} />));
+      sections = words.map((word) => (
+        <Tooltip followCursor title={`${Math.ceil(word.confidence)}%`}>
+          <div style={getRectangle(word, container)} />
+        </Tooltip>
+      ));
     }
     return sections;
   }, [highlightAction, textBlocks, container]);
