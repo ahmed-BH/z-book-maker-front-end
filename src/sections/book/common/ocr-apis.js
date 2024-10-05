c1d578f3ea3c58ae38315fca99b2c976da5c68c9
abenhamouda <abenhamouda@iobeya.com>
2024-10-05 21:54:06 +0100

    ♻️ replace fetch with axios

--- src/sections/book/common/ocr-apis.js
+++ src/sections/book/common/ocr-apis.js
@@ -1,13 +1,10 @@
-import { baseURL } from 'src/utils/constants';
+import { axiosAPIInstance } from 'src/utils/constants';
 
 export class OCRAPI {
   static OCRImage(image) {
     const formData = new FormData()
     formData.append('imgData', image);
 
-    return fetch(`${baseURL}/ocr`, {
-      method: 'post',
-      body: formData
-    });
+    return axiosAPIInstance.post(`/page/ocr`, formData);
   }
 }
