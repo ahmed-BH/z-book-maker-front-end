7da237e7b02fe26fd61b7b3b08796e40ca46e193
abenhamouda <abenhamouda@iobeya.com>
2024-10-11 22:23:10 +0100

    :recycle: pass a file object instead of base64 image to /ocr

--- src/sections/book/common/ocr-apis.js
+++ src/sections/book/common/ocr-apis.js
@@ -1,10 +1,19 @@
+import { v4 as uuidv4 } from 'uuid';
+
 import { axiosAPIInstance } from 'src/utils/constants';
 
-export class OCRAPI {
-  static OCRImage(image) {
-    const formData = new FormData()
-    formData.append('imgData', image);
+import { OCRUtils } from 'src/sections/book/common/ocr-utils';
 
-    return axiosAPIInstance.post(`/page/ocr`, formData).then((response) => response.data);
+export class OCRAPI {
+  static OCRImage(base64Image) {
+    const fileFromImage = OCRUtils.dataURLtoFile(base64Image, uuidv4());
+    const formData = new FormData()
+    formData.append('imgData', fileFromImage);
+
+    return axiosAPIInstance.post(`/page/ocr`, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    }).then((response) => response.data);
   }
 }
--- src/sections/book/common/ocr-utils.js
+++ src/sections/book/common/ocr-utils.js
@@ -48,4 +48,17 @@ export class OCRUtils {
     });
     return series;
   }
+
+  static dataURLtoFile(dataurl, filename) {
+    const arr = dataurl.split(',');
+    const mime = arr[0].match(/:(.*?);/)[1];
+    const bstr = atob(arr[arr.length - 1]); 
+    let n = bstr.length; 
+    const u8arr = new Uint8Array(n);
+    while(n){
+      n -= 1;
+      u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new File([u8arr], filename, { type:mime });
+}
 };
